713931ade27dc6229c56362fa2810251c8fbed76
Caio Felicio <caiofelicio1711@gmail.com>
2023-05-30 12:38:14 -0300

    edit e delete

--- js/script.js
+++ js/script.js
@@ -7,62 +7,103 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldTitle;
+
 // Funções
 
 const saveTarefa = (title) => {
-    const tarefa = document.createElement("div");
-    tarefa.classList.add("tarefa");
+	const tarefa = document.createElement("div");
+	tarefa.classList.add("tarefa");
+	
+	const tarefaTitle = document.createElement("h3");
+	tarefaTitle.innerText = title;
+	tarefa.appendChild(tarefaTitle);
+	
+	const doneBtn = document.createElement("button");
+	doneBtn.classList.add("done-tarefa");
+	doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+	tarefa.appendChild(doneBtn);
+	
+	const editBtn = document.createElement("button");
+	editBtn.classList.add("edit-tarefa");
+	editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+	tarefa.appendChild(editBtn);
+	
+	const deleteBtn = document.createElement("button");
+	deleteBtn.classList.add("delete-tarefa");
+	deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+	tarefa.appendChild(deleteBtn);
+	
+	tarefasList.appendChild(tarefa);
+	
+	addInput.value = "";
+	addInput.focus();
+}
 
-    const tarefaTitle = document.createElement("h3");
-    tarefaTitle.innerText = title;
-    tarefa.appendChild(tarefaTitle);
+const toggleForms = () => {
+	editForm.classList.toggle("hide");
+	addForm.classList.toggle("hide");  
+	tarefasList.classList.toggle("hide");  
+}
 
-    const doneBtn = document.createElement("button");
-    doneBtn.classList.add("done-tarefa");
-    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
-    tarefa.appendChild(doneBtn);
-    
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit-tarefa");
-    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
-    tarefa.appendChild(editBtn);
-    
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete-tarefa");
-    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-    tarefa.appendChild(deleteBtn);
+const updateTarefa = (input) => {
+	const tarefas = tarefasList.querySelectorAll(".tarefa");
 
-    tarefasList.appendChild(tarefa);
+	tarefas.forEach(tarefa => {
+		let title = tarefa.querySelector("h3");
 
-    addInput.value = "";
-    addInput.focus();
+		if(title.innerText === oldTitle){
+			title.innerText = input;
+		}
+
+	});
 }
 
 // Eventos
 
 addForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const inputValue = addInput.value;
-
-    if(inputValue){
-        saveTarefa(inputValue);
-    }
+	e.preventDefault();
+	
+	const inputValue = addInput.value;
+	
+	if(inputValue){
+		saveTarefa(inputValue);
+	}
 });
 
 tarefasList.addEventListener("click", (e) => {
-    const targetBtn = e.target;
-    const tarefaBtn = targetBtn.closest("div");
+	const targetBtn = e.target;
+	const tarefaBtn = targetBtn.closest("div");
+	oldTitle = tarefaBtn.querySelector("h3").innerText;
+	
+	if(targetBtn.classList.contains("done-tarefa")){
+		tarefaBtn.classList.toggle("done");
+		targetBtn.classList.toggle("done-btn");
+	}
+	
+	if(targetBtn.classList.contains("edit-tarefa")){
+		toggleForms();
+	}
+	
+	if(targetBtn.classList.contains("delete-tarefa")){
+		tarefaBtn.remove();
+	}
+});
 
-    if(targetBtn.classList.contains("done-tarefa")){
-        tarefaBtn.classList.add("done");
-    }
+cancelEditBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	
+	toggleForms();
+});
 
-    if(targetBtn.classList.contains("edit-tarefa")){
-        
-    }
+editForm.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-    if(targetBtn.classList.contains("delete-tarefa")){
-        
-    }
+	const inputValue = editInput.value;
+
+	if(inputValue){
+		updateTarefa(inputValue);
+	}
+
+	toggleForms();
 });
